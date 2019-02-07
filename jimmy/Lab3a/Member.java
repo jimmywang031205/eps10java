5dba9c0fc255bdf834d652c278fa0e06053d5222
Jimmy Wang <jimmy.wang.031205@gmail.com>
2019-02-07 09:18:24 -0800

    changes

--- jimmy/Lab3a/Member.java
+++ jimmy/Lab3a/Member.java
@@ -18,13 +18,33 @@ public class Member
     
     public void member(String _firstName, String _lastName, int _yearBorn, int _yearJoined)
     {
+        setFirstName(_firstName);
+        setLastName(_lastName);
+        setYearBorn(_yearBorn);
+        setYearJoined(_yearJoined);
+    }
+    
+    public void setFirstName(String _firstName)
+    {
         firstName = _firstName;
+    }
+    
+    public void setLastName(String _lastName)
+    {
         lastName = _lastName;
+    }
+    
+    public void setYearBorn(int _yearBorn)
+    {
         if (_yearBorn < 0)  {
             yearBorn = (-1*_yearBorn);
         } else {
             yearBorn = _yearBorn;
         }
+    }
+    
+    public void setYearJoined(int _yearJoined)
+    {
         if (_yearJoined < 0)  {
             yearJoined = (-1*_yearJoined);
         } else {
@@ -32,5 +52,53 @@ public class Member
         }
     }
     
-    public void 
+    public String getFirstName()
+    {
+        return firstName;
+    }
+    
+    public String getLastName()
+    {
+        return lastName;
+    }
+    
+    public int getYearBorn()
+    {
+        return yearBorn;
+    }
+    
+    public int getYearJoined()
+    {
+        return yearJoined;
+    }
+    
+    public String returnFullName()
+    {
+        return (firstName+" "+lastName);
+    }
+    
+    public int calculate(int i)
+    {
+        int temp = CURRENT_YEAR - i;
+        return temp;
+    }
+    
+    public boolean qualifiesForDiscount()
+    {
+        if(calculate(yearBorn) < 12 && calculate(yearJoined) >= 2)  {
+            return true;
+        } else if (calculate(yearBorn) >= 65)   {
+            return true;
+        } else if (calculate(yearJoined) >= 10) {
+            return true;
+        } else  {
+            return false;
+        }
+    }
+
+    public void display()
+    {
+        System.out.println("Full Name: "+returnFullName());
+        System.out.println("Discount: "+qualifiesForDiscount());
+    }
 }
