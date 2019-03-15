772253ddcd5a10d011049d12f3199d20e737a028
0xC00005 <hecknet@163.com>
2019-03-15 13:37:50 -0700

    Add files via upload

--- max/Lab12/MyClass.java
+++ max/Lab12/MyClass.java
@@ -34,21 +34,22 @@ public class MyClass {
             String jud_gender;
             String jud_pro;
             boolean result=false;
-            System.out.print("grade: ");jud_grade=MyInput.getInt();
-            System.out.print("gender: ");jud_gender=MyInput.getString();
-            System.out.print("programming 12: ");jud_pro=MyInput.getString();
+            System.out.print("Grade: ");jud_grade=MyInput.getInt();
+            System.out.print("Gender(F/M): ");jud_gender=MyInput.getString();
+            System.out.print("Programming 12(yes/no): ");jud_pro=MyInput.getString();
             if(jud_pro.equals("yes"))result=true;
 
             for(Students element:classList)
             {
-                if(element.getGrade()==jud_grade)
-                    if(element.getGender()==jud_gender)
-                     if (element.getProgramming_12_Student()==result)
-                     {
-                         System.out.print(element.getFirstName()+" "+element.getLastName()+" "+element.getStudent_Number()+" "+element.getGrade()+" "+element.getGender()+" Programming");
-                         if(element.getProgramming_12_Student())System.out.println("12");
-                         else System.out.println("11");
-                     }
+                if(element.getGrade()==jud_grade) 
+                    if(element.getGender().toUpperCase().equals(jud_gender.toUpperCase()))
+                        if (element.getProgramming_12_Student()==result)
+                        {
+                            System.out.print(element.getFirstName()+" "+element.getLastName()+" "+element.getStudent_Number()+" "+element.getGrade()+" "+element.getGender()+" Programming");
+                            if(element.getProgramming_12_Student())System.out.println("12");
+                            else System.out.println("11");
+                        }
+                 
             }
 
         }
